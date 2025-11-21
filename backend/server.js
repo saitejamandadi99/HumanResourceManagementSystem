1b01eead3faac9ed06fdc835094433f2e29ea8cf
saitejamandadi99 <saitejamandadi99>
2025-11-21 11:22:07 +0530

    added the basic structure of the backend and install axios for fetching and completed basic server.js file to connect to localhost

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res)=>{
+    res.send('Server is running in the backend')
+    console.log('Server is running in the backend')
+})
+
+const PORT = process.env.PORT || 5000 
+app.listen(PORT,()=>{
+    console.log(`server is running in http://localhost:${PORT}`)
+})
