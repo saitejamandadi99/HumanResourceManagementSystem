e5c04bd9298130622ffe86a47e97211016a95d6b
saitejamandadi99 <saitejamandadi99>
2025-11-21 13:24:26 +0530

    build users table and added id in the organisations

--- backend/models/organisations.js
+++ backend/models/organisations.js
@@ -1,6 +1,11 @@
 const {DataTypes} = require('sequelize')
 const sequelize = require('../db')
 const Organisations = sequelize.define('Organisations', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
     name:{
         type: DataTypes.STRING,
         allowNull:false,
---
+++ backend/models/users.js
@@ -0,0 +1,41 @@
+const {DataTypes} = require('sequelize')
+const sequelize = require('../db')
+const Users = sequelize.define('Users', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    organisation_id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        references:{
+            model:"Organisations",
+            key:'id',
+        }
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true
+    },
+    password_hash:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    name:{
+        type: DataTypes.STRING,
+
+    },
+    
+    created_at:{
+        type:DataTypes.DATE,
+        defaultValue:DataTypes.NOW,
+    }
+});
+
+Users.sync()
+.then(()=>console.log('Users table created'))
+.catch(err=>console.log('Users table failed to create', err))
+
+module.exports = Users
