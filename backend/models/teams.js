0083fd3a250599500b308168434021ac052b1402
saitejamandadi99 <saitejamandadi99>
2025-11-21 13:34:33 +0530

    teams model is created

---
+++ backend/models/teams.js
@@ -0,0 +1,34 @@
+const {DataTypes} = require('sequelize')
+const sequelize = require('../db')
+const Teams  = sequelize.define('Teams ', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    organisation_id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        references:{
+            model:"Organisations",
+            key:'id',
+        }
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    }
+});
+
+Teams .sync()
+.then(()=>console.log('Teams  table created'))
+.catch(err=>console.log('Teams  table failed to create', err))
+
+module.exports = Teams 
